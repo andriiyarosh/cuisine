a9df1f513103cfdca0fc055829bf0b860ab360b4
cr9ck <andreyarosh29@gmail.com>
2020-09-25 00:48:24 +0300

    Boot receiver registered. On back pressed handler added. Home Button handled.

--- app/src/main/java/com/work/cuisine/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/work/cuisine/presentation/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.work.cuisine.presentation.activity
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.work.cuisine.R
 import com.work.cuisine.base.BaseActivity
 import com.work.cuisine.presentation.activity.NetworkStateObservable.NetworkState
@@ -37,9 +41,26 @@ class MainActivity : BaseActivity(), NetworkStateObservable {
         connectivityManager.registerDefaultNetworkCallback(networkCallback)
     }
 
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
     override fun onStop() {
         super.onStop()
         connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    @SuppressLint("RestrictedApi")
+    override fun onBackPressed() {
+        if (findNavController(R.id.hostFragment).previousBackStackEntry == null)
+            return
+        super.onBackPressed()
     }
 
     override fun subscribe(observer: Observer<NetworkState>) {
--- app/src/main/java/com/work/cuisine/receiver/BootCompleteReceiver.kt
+++ app/src/main/java/com/work/cuisine/receiver/BootCompleteReceiver.kt
@@ -5,9 +5,9 @@ import android.content.Context
 import android.content.Intent
 import com.work.cuisine.presentation.activity.MainActivity
 
-class BootCompleteReceiver: BroadcastReceiver() {
+class BootCompleteReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        context?.startActivity(Intent(context, MainActivity::class.java))
+    override fun onReceive(context: Context, intent: Intent) {
+        context.startActivity(Intent(context, MainActivity::class.java))
     }
 }
